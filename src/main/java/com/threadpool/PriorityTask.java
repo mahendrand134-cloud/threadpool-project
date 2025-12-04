76c017b15cf42cb6a449279420d314a8141267d3
mahendrand134-cloud <mahendrand134@gmail.com>
2025-12-04 14:24:59 +0530

    Add files via upload

---
+++ src/main/java/com/threadpool/PriorityTask.java
@@ -0,0 +1,21 @@
+package com.threadpool;
+
+public class PriorityTask implements Comparable<PriorityTask>, Runnable {
+    private final int priority;
+    private final Runnable task;
+
+    public PriorityTask(int priority, Runnable task) {
+        this.priority = priority;
+        this.task = task;
+    }
+
+    @Override
+    public int compareTo(PriorityTask other) {
+        return Integer.compare(other.priority, this.priority);
+    }
+
+    @Override
+    public void run() {
+        task.run();
+    }
+}
